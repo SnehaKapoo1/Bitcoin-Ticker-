8ee426acb5d8c25cf38a27c800b58128803204f4
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-01-10 14:06:24 +0530

    Ui changed

---
+++ lib/price_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  const PriceScreen({Key? key}) : super(key: key);
+
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Coin Ticker😛'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            color: Colors.lightBlue,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(bottom: 30.0),
+            child: null,
+          )
+        ],
+      ),
+    );
+  }
+}
